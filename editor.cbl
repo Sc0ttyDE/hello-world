5317c666fe59ea05c88dba0ff1f0b5077c745f01
Sc0ttyDE <sdavis@de.ibm.com>
2017-02-20 12:23:03 +0100

    New lines OK but what about changed lines

--- editor.cbl
+++ editor.cbl
@@ -1,7 +1,7 @@
 You call this an editor ? What a crock
-MOVE variable to variabl2
+MOVE variabl2 to variabl3
 Well for some reason movce and call are blue when saved but not blue in the editor ?????
 In the naster this line of code was empty but the cursor was also indented about 10 spaces.
       No idea why well and you can see here the same behaviour
-If variable = 10
-      then variabl2 = -2
+If variabl2 = 10
+      then variabl3 = -2
